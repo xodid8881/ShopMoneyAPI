db3689c0cd64ab73842163b55b563f3f313293d5
Hwa_Baeg_ <aoadid8881@naver.com>
2020-09-06 01:11:21 +0900

    Update EventListener.php

--- ShopMoneyAPI/src/ShopMoneyAPI/EventListener.php
+++ ShopMoneyAPI/src/ShopMoneyAPI/EventListener.php
@@ -4,10 +4,8 @@ declare(strict_types=1);
 namespace ShopMoneyAPI;
 
 use pocketmine\event\Listener;
-use NPCShop\entity\NPCEntity;
 use pocketmine\Player;
 use pocketmine\utils\TextFormat;
-use CoinAPI\CoinAPI;
 
 class EventListener implements Listener
 {
